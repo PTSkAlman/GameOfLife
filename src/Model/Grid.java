5b02c8ce4ed5477cdecb4112bd8f6049764400f0
PTSkAlman <joel.abrahamsson@elev.ga.ntig.se>
2021-12-06 09:13:07 +0100

    nästan färdig

--- src/Model/Grid.java
+++ src/Model/Grid.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Grid {
 
     private int[][] grid;
+    private int[][] nextGrid;
+    private int sum = 0;
 
     public Grid() {
         grid = new int[][]{
@@ -17,41 +19,50 @@ public class Grid {
     }
 
     public void nextGrid() {
+        nextGrid = grid;
         for (int i = 0 ; i < grid.length ; i++) {
             for (int j = 0 ; j < grid[i].length ; j++) {
                 checkNeighbor(j, i);
             }
         }
+        grid = nextGrid;
     }
 
     private void checkNeighbor(int x, int y) {
-        int sum = 0;
+        // UL
         if (x - 1 >= 0 && y - 1 >= 0 && grid[y-1][x-1] == 1) {
             sum++;
+        // UM
         } else if (x >= 0 && y - 1 >= 0 && grid[y-1][x] == 1) {
             sum++;
+        // UR
         } else if (x + 1 < grid.length && y - 1 >= 0 && grid[y-1][x+1] == 1) {
             sum++;
+        // ML
         } else if (x - 1 >= 0 && y >= 0 && grid[y][x-1] == 1) {
             sum++;
+        // MR
         } else if (x + 1 < grid.length && y >= 0 && grid[y][x+1] == 1) {
             sum++;
+        // DL
         } else if (x - 1 >= 0 && y + 1 < grid.length && grid[y+1][x-1] == 1) {
             sum++;
+        // DM
         } else if (x >= 0 && y + 1 < grid.length && grid[y+1][x] == 1) {
             sum++;
+        // DR
         } else if (x + 1 < grid.length && y + 1 < grid.length && grid[y+1][x+1] == 1) {
             sum++;
         }
         if (sum == 2 || sum == 3) {
-            grid[y][x] = 1;
+            nextGrid[y][x] = 1;
         } else {
-            grid[y][x] = 0;
+            nextGrid[y][x] = 0;
         }
     }
 
     public void checkIndex(int x, int y) {
-        if (grid[y][x] == 0) {
+        if (nextGrid[y][x] == 0) {
             System.out.println("dead");
         } else if (grid[y][x] == 1) {
             System.out.println("alive");
@@ -60,7 +71,8 @@ public class Grid {
 
     public void startGrid(Cell[] cell) {
         for (int i = 0; i < cell.length; i++) {
-            //cell[i];
+            cell[i] = new Cell(i,i);
+            grid[cell[i].getX()][cell[i].getY()] = 1;
         }
         //int x = cell.getX();
         //int y = cell.getY();
